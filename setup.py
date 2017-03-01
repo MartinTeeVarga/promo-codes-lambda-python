f056f3073cf48a573b3a35d7c23868f81818af9d
Martin Varga <m.varga@pks.com.au>
2017-03-02 09:21:27 +1100

    added setup script

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='promo-codes-lambda',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='https://github.com/sm4/promo-codes-lambda-python',
+      author='sm4',
+      license='MIT',
+      install_requires=[
+          'boto3', 'mock'
+      ])
