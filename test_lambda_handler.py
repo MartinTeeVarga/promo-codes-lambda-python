8fd34b51cc66d332fe09ce800319ba6d07213fad
Martin Varga <m.varga@pks.com.au>
2017-03-02 09:21:37 +1100

    added unit test

---
+++ test_lambda_handler.py
@@ -0,0 +1,8 @@
+import unittest
+import mock
+import lambda_handler
+
+class TestLambda_handler(unittest.TestCase):
+    def test_lambda_handler(self):
+        lambda_handler.lambda_handler("hello", "world")
+        self.fail()
