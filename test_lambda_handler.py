c52abfcff9982169f44d315f85f1074e4aac7e93
sm4 <sm4@kul.is>
2017-03-02 19:08:55 +1100

    added if __name__ == '__main__': to run the tests

--- test_lambda_handler.py
+++ test_lambda_handler.py
@@ -6,3 +6,6 @@ class TestLambda_handler(unittest.TestCase):
     def test_lambda_handler(self):
         lambda_handler.lambda_handler("hello", "world")
         self.fail()
+
+if __name__ == '__main__':
+    unittest.main()
